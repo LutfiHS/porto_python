e66c597c2465b26c5b3754f87e8ad7b38043b84b
lutfiHs <lutfihandi@gmail.com>
2024-06-12 06:10:34 +0700

    add: index.ipynb and data_sql.sql

---
+++ data_sql.sql
@@ -0,0 +1,11 @@
+-- Regular
+DECLARE @tahun AS VARCHAR(8)
+DECLARE @bulan AS VARCHAR(8)
+SET @tahun = '2024';
+SET @bulan = '05'
+SELECT
+    DISTINCT 
+FROM
+WHERE
+    c.tahun = @tahun
+    and c.bulan = @bulan
